787ecd00be04d392ef590153a3709b9deda82829
Your Name <ubaydullayev@samtuit.uz>
2022-01-28 19:04:44 +0500

    count_all

--- count_all.py
+++ count_all.py
@@ -7,4 +7,17 @@ def count_all(txt):
     Returns:
         dict: dictionary with letters and digits
     """
-    return
+    count_letters = 0
+    count_digits = 0
+    idx = 0
+    while idx < len(txt):
+        if txt[idx].isdigit():
+            count_digits += 1
+        if txt[idx].isalpha():
+            count_letters += 1
+        idx += 1
+    answer = {'LETTERS': count_letters, 'DIGITS': count_digits}
+    return answer
+
+txt = "python foundations 2022"
+print(count_all(txt))
